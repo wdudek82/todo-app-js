c7b3cf6e1177edcc0408330a180f78a2fda0f9cc
Wojciech Dudek <wdudek82@gmail.com>
2017-05-28 10:08:36 +0200

    Created project structure, first js code to add, and reprint list of tasks on page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-let a = 5;
 
-console.log(a);
+class ToDo {
+    constructor() {
+        this.listOfTasks = [];
+        this.todoList = document.getElementById('todo-list');
+    }
+
+    addTask() {
+        let task = document.getElementById('add-task');
+        let taskContent = task.value;
+
+        if (taskContent) {
+            this.listOfTasks.push(taskContent);
+            this.redrawList(taskContent);
+        }
+    }
+
+    redrawList(content) {
+        console.log('redrawing...');
+        this.todoList.innerHtml = null;
+
+        for (let task of this.listOfTasks) {
+            let newTask = document.createElement('li');
+            let textNode = document.createTextNode(content);
+            newTask.appendChild(textNode);
+            newTask.id = this.listOfTasks.indexOf(task);
+            this.todoList.appendChild(newTask);
+
+            console.log(task);
+        }
+    }
+
+}
+
+let obj = new ToDo();
