f0bd0b2abd79e97789afb8238c77e74d77386b1e
Wojciech Dudek <wdudek82@gmail.com>
2017-05-29 23:41:03 +0200

    Added checkbox to tasks than allow marking tasks as completed, removed style from ul

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class ToDo {
         let newTask = inputContent.value;
 
         if (newTask) {
-            this.listOfTasks.push(newTask);
+            this.listOfTasks.push([newTask, 0]);
             inputContent.value = '';
             this.listTasks();
         }
@@ -33,7 +33,6 @@ class ToDo {
 
         for (let [taskIndex, task] of this.listOfTasks.entries()) {
             let newTaskNode = ToDo.createNode(taskIndex, task);
-            // this.todoList.appendChild(newTaskNode);
             this.todoList.insertBefore(newTaskNode, this.todoList.firstChild);
         }
 
@@ -42,15 +41,24 @@ class ToDo {
 
     static createNode(nodeId, nodeContent) {
         let newNode = document.createElement('li');
-        let textNode = document.createTextNode(' ' + nodeContent);
+        let textNode = document.createTextNode(' ' + nodeContent[0]);
+        let textColor = nodeContent[1] ? 'blue' : 'red';
+        newNode.style.color = textColor;
         newNode.id = nodeId;
 
         // TODO: add some way to mark tasks as completed
+        let isCompleted = document.createElement('input');
+        isCompleted.setAttribute('type', 'checkbox');
+        isCompleted.setAttribute('id', `taskCheckbox${nodeId}`);
+        isCompleted.setAttribute('onClick', `obj.changeStatus(${nodeId})`);
+        newNode.appendChild(isCompleted);
+
         let editButton = ToDo.createButtonNode('edit', `obj.updateTask(${nodeId})`);
         let deleteButton = ToDo.createButtonNode('x', `obj.deleteTask(${nodeId})`);
+        editButton.style.color = 'black';
+        deleteButton.style.color = 'black';
         newNode.appendChild(editButton);
         newNode.appendChild(deleteButton);
-
 
         newNode.appendChild(textNode);
 
@@ -64,6 +72,11 @@ class ToDo {
         button.setAttribute('onClick', event);
 
         return button;
+    }
+
+    changeStatus(taskId) {
+        let taskCheckbox = document.getElementById(`taskCheckbox${taskId}`).checked;
+        document.getElementById(taskId).style.color = taskCheckbox ? 'green' : 'red';
     }
 
     consolePrint() {
