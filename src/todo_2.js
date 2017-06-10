b89e6f8d6ee7f2f23a4d7d963e8226efc4457542
Wojciech Dudek <wdudek82@gmail.com>
2017-06-10 17:44:40 +0200

    Added buttons & logic for delete, edit + save/cancel button & first draft of logic behind it

--- src/todo_2.js
+++ src/todo_2.js
@@ -20,6 +20,61 @@ class ToDo {
 
     }
 
+    editTask(taskId) {
+        // redrawing allows editing one item at a time
+        this.displayList();
+
+        let task = document.getElementById('task-item-'+taskId);
+
+        console.log(task);
+        for (let child of task.childNodes) {
+            if (child.classList.contains('task-edit')) {
+                console.log(child);
+                child.remove();
+
+                let saveNode = this.createItem('div', {
+                    classNames: ['task-save'],
+                    attributes: [
+                        ['onClick', `todo.saveTask(${taskId})`]
+                    ]
+                });
+                let saveIcon = this.createItem('i', {
+                    classNames: ['fa', 'fa-floppy-o'],
+                    attributes: [['aria-hidden', 'true']]
+                });
+                saveNode.appendChild(saveIcon);
+
+                let cancelNode = this.createItem('div', {
+                    classNames: ['task-cancel'],
+                    attributes: [
+                        ['onClick', `todo.cancelEdit('task-item-${taskId}')`]
+                    ]
+                });
+                let cancelIcon = this.createItem('i', {
+                    classNames: ['fa', 'fa-ban'],
+                    attributes: [['aria-hidden', 'true']],
+                });
+                cancelNode.appendChild(cancelIcon);
+
+                task.appendChild(saveNode);
+                task.appendChild(cancelNode);
+            }
+        }
+    }
+
+    saveTask(taskId) {
+        console.log('savind task ' + taskId);
+    }
+
+    cancelEdit(taskItemId) {
+        console.log('cancelling editing task ' + taskItemId)
+    }
+
+    deleteTask(taskId) {
+        this.state.list.splice(taskId, 1);
+        this.displayList();
+    }
+
     taskClicked(taskId) {
         // Changes 1 to 0 and vice versa
         this.state.list[taskId][1] = 1 - this.state.list[taskId][1];
@@ -39,32 +94,84 @@ class ToDo {
         let listOfTasks = this.state.list;
 
         for (let task of listOfTasks.entries()) {
-            console.log(task);
-
             let taskIndex = task[0];
             let taskContent = task[1][0];
             let taskCompleted = task[1][1];
+            let taskId = 'task-' + taskIndex;
 
+            let taskNode = this.createItem('div', {id: 'task-item-' + taskIndex, classNames: ['task-item']});
 
-            let newNode = document.createElement('div');
-            let textNode = document.createTextNode(taskContent);
+            let deleteNode = this.createItem('div', {
+                classNames: ['task-delete'],
+                attributes: [['onClick', `todo.deleteTask(${taskIndex})`]]
+            });
+            let deleteIcon = this.createItem('i', {
+                classNames: ['fa', 'fa-trash-o'],
+                attributes: [['aria-hidden', 'true']]
+            });
+            deleteNode.appendChild(deleteIcon);
 
-            newNode.classList.add('task');
+            let contentNode = this.createItem('div', {
+                text: taskContent,
+                id: taskId,
+                classNames: ['task-content']
+            });
+
+            let editNode = this.createItem('div', {
+                classNames: ['task-edit'],
+                attributes: [['onClick', `todo.editTask(${taskIndex})`]]
+            });
+            let editIcon = this.createItem('i', {
+                classNames: ['fa', 'fa-pencil'],
+                attributes: [['aria-hidden', 'true']]
+            });
+            editNode.appendChild(editIcon);
+
             if (taskCompleted) {
-                newNode.classList.add('completed')
+                contentNode.classList.add('completed')
             } else {
-                newNode.classList.remove('completed');
+                contentNode.classList.remove('completed');
             }
 
-            newNode.id = 'task-' + taskIndex;
-            newNode.setAttribute('onClick', `todo.taskClicked(${taskIndex})`);
-            newNode.appendChild(textNode);
+            contentNode.setAttribute('onClick', `todo.taskClicked(${taskIndex})`);
 
-            let ol = this.state.uList;
-            ol.insertBefore(newNode, ol.firstChild);
+            taskNode.appendChild(deleteNode);
+            taskNode.appendChild(contentNode);
+            taskNode.appendChild(editNode);
+
+            let list = this.state.uList;
+            list.insertBefore(taskNode, list.firstChild);
 
         }
 
+    }
+
+    createItem(selector, {
+        text = null,
+        id = null,
+        classNames = null,
+        attributes = null}) {
+        let newNode = document.createElement(selector);
+
+        if (text) {
+            let newNodeText = document.createTextNode(text);
+            newNode.appendChild(newNodeText);
+        }
+        if (id) {
+            newNode.id = id;
+        }
+        if (classNames) {
+            for (let className of classNames) {
+                newNode.classList.add(className);
+            }
+        }
+        if (attributes) {
+            for (let [attrName, attrValue] of attributes) {
+                newNode.setAttribute(attrName, attrValue);
+            }
+        }
+
+        return newNode;
     }
 
     clearList() {
